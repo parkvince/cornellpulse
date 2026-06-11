b96404f25294174bcac0ea2a3874944afa138959
parkvince <seho.vince@gmail.com>
2026-06-11 03:39:12 -0400

    add capacitor android setup

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cornellpulse.app',
+  appName: 'CornellPulse',
+  webDir: 'dist'
+};
+
+export default config;
