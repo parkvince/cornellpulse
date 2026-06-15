d41b5054a5515c0e95f3127dfaa95cb513752b85
parkvince <seho.vince@gmail.com>
2026-06-14 23:55:40 -0400

    add app icon and splash screen

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cornellpulse.app',
   appName: 'CornellPulse',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500,
+      backgroundColor: "#0f0f0f",
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
